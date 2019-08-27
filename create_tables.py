e7cb95d84cc337fbfc87bbafde16da41265804cc
Robert Osterburg <rob@Roberts-iMac.hsd1.co.comcast.net>
2019-08-27 13:19:20 -0600

    building connection string changes

--- create_tables.py
+++ create_tables.py
@@ -4,7 +4,7 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def drop_tables(cur, conn):
-    for query in drop_table_queries:
+for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
@@ -17,9 +17,14 @@ def create_tables(cur, conn):
 
 def main():
     config = configparser.ConfigParser()
-    config.read('dwh.cfg')
+    config.read("dwh.cfg")
 
-    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+    connect_str = "host={} dbname={} user={} password={} port={}".format(
+            *config["CLUSTER"].values()
+
+    conn = psycopg2.connect(connect_str)
+        )
+    )
     cur = conn.cursor()
 
     drop_tables(cur, conn)
