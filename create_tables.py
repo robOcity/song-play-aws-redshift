9d6876be37551493ccec2176acd208d95bf5d708
Robert Osterburg <rob@Roberts-iMac.hsd1.co.comcast.net>
2019-08-27 14:14:46 -0600

    fixing vscode indentation issues

--- create_tables.py
+++ create_tables.py
@@ -4,7 +4,7 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def drop_tables(cur, conn):
-for query in drop_table_queries:
+    for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
@@ -18,11 +18,9 @@ def main():
     config = configparser.ConfigParser()
     config.read("dwh.cfg")
 
-    connect_str = "host={} dbname={} user={} password={} port={}".format(
-            *config["CLUSTER"].values())
+    connect_str = "host={} dbname={} user={} password={} port={}".format(*config["CLUSTER"].values())
 
     conn = psycopg2.connect(connect_str)
-
 
     cur = conn.cursor()
 
