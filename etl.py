557778562915faabc33ec3290b979831f62b2524
Robert Osterburg <rob@Roberts-iMac.hsd1.co.comcast.net>
2019-08-27 13:17:56 -0600

    wrap long lines

--- etl.py
+++ etl.py
@@ -1,9 +1,10 @@
 import configparser
 import psycopg2
+import sys
 from sql_queries import copy_table_queries, insert_table_queries
 
 
-def load_staging_tables(cur, conn):
+def load_staging_tables(cur, conn, config):
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
@@ -17,12 +18,15 @@ def insert_tables(cur, conn):
 
 def main():
     config = configparser.ConfigParser()
-    config.read('dwh.cfg')
-
-    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+    config.read("dwh.cfg")
+    fmt_config_str = "host={} dbname={} user={} password={} port={}".format(
+        *config["CLUSTER"].values()
+    )
+    print(f"fmt_config_str={fmt_config_str}")
+    conn = psycopg2.connect(fmt_config_str)
     cur = conn.cursor()
-    
-    load_staging_tables(cur, conn)
+
+    load_staging_tables(cur, conn, config)
     insert_tables(cur, conn)
 
     conn.close()
