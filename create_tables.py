bcb81969ba1d01cd97ba87e51b040365519ab4e4
Rob Osterburg <robert.osterburg@gmail.com>
2019-08-30 15:02:41 -0600

    adding doc strings

--- create_tables.py
+++ create_tables.py
@@ -1,22 +1,31 @@
+"""
+Connects to AWS Redshift and drops and re-creates all tables.
+"""
+
 from sql_queries import create_table_queries, drop_table_queries
 from utils import connect
 
 
 def drop_tables(cur, conn):
+    """Drops all tables from the database"""
+
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
-        print(f'\nRunning: {query}')
+        print(f"\nRunning: {query}")
 
 
 def create_tables(cur, conn):
+    """Creates both staging and star-schema tables"""
+
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
-        print(f'\nRunning: {query}')
+        print(f"\nRunning: {query}")
 
 
 def main():
+    """Drops and re-creates all tables"""
     cur, conn = connect()
     drop_tables(cur, conn)
     create_tables(cur, conn)
--- etl.py
+++ etl.py
@@ -1,39 +1,46 @@
+"""Extracts, transforms and loads data from S3 to star-schema tables."""
+
 from sql_queries import copy_table_queries, insert_table_queries
 from utils import connect
 
 
 def load_staging_tables(cur, conn):
+    """Extracts data from JSON files on S3 inserting data into staging tables"""
+
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
-        print(f'\nRunning: {query}')
+        print(f"\nRunning: {query}")
 
 
 def insert_tables(cur, conn):
+    """Transforms and loads data from staging to star schema tables"""
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
-        print(f'\nRunning: {query}')
+        print(f"\nRunning: {query}")
 
 
 def get_command():
-    cmd = ''
-    while cmd not in [ 'L', 'I']:
-        cmd = input('\nL - load\nI - insert\nCommand: ')[0].upper()
+    """Enables user to either extract (load) for insert (transform & load) the data"""
+    cmd = ""
+    while cmd not in ["L", "I"]:
+        cmd = input("\nL - load\nI - insert\nCommand: ")[0].upper()
     return cmd
 
 
 def main():
+    """Extracts or transforms and loads the data at the users request"""
 
     cmd = get_command()
-    if cmd == 'L':
+    if cmd == "L":
         # load data
-        print('\nLoading data (~20 minutes)')
+        print("\nLoading data (~20 minutes)")
         cur, conn = connect()
         load_staging_tables(cur, conn)
-    elif cmd == 'I':
-        # insert data 
-        print('\nInserting data (~2 minutes)')
+    elif cmd == "I":
+        # insert data
+        print("\nInserting data (~2 minutes)")
         cur, conn = connect()
         insert_tables(cur, conn)
 
