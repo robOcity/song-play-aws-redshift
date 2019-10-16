7dce159615255e0f3a131af3da7799af0987ab13
robOcity <robert.osterburg@gmail.com>
2019-10-16 13:23:22 -0600

    feature: create and quit commands added

--- etl.py
+++ etl.py
@@ -1,6 +1,7 @@
 """Extracts, transforms and loads data from S3 to star-schema tables."""
 
 from sql_queries import copy_table_queries, insert_table_queries
+from create_tables import create_tables
 from utils import connect
 
 
@@ -33,26 +34,38 @@ def insert_tables(cur, conn):
 def get_command():
     """Loads new data and updates existing (transform & load) data."""
     cmd = ""
-    while cmd not in ["L", "I"]:
-        cmd = input("\nL - load\nI - insert\nCommand: ")[0].upper()
+    while cmd not in ["C", "L", "I", "Q"]:
+        cmd = input("\nC - create\nL - load\nI - insert\nQ - quit\nCommand: ")[0].upper()
     return cmd
 
 
 def main():
     """Extracts or transforms and loads the data at the users request."""
-    cmd = get_command()
-    if cmd == "L":
-        # load data
-        print("\nLoading data (~20 minutes)")
-        cur, conn = connect()
-        load_staging_tables(cur, conn)
-    elif cmd == "I":
-        # insert data
-        print("\nInserting data (~2 minutes)")
-        cur, conn = connect()
-        insert_tables(cur, conn)
 
-    conn.close()
+    while True:
+        cmd = get_command()
+
+        if cmd == "C":
+            print("\nDrop then re-create all tables (~1 minute)")
+            cur, conn = connect()
+            create_tables(cur, conn)
+
+        elif cmd == "L":
+            # load data
+            print("\nLoading data (~20 minutes)")
+            cur, conn = connect()
+            load_staging_tables(cur, conn)
+
+        elif cmd == "I":
+            # insert data
+            print("\nInserting data (~2 minutes)")
+            cur, conn = connect()
+            insert_tables(cur, conn)
+
+        elif cmd == "Q":
+            break
+
+        conn.close()
 
 
 if __name__ == "__main__":
